dd87cfdb3f31f6026d49fb70f258e462b0ca6dd4
Arungeorge103 <153169302+Arungeorge103@users.noreply.github.com>
2024-01-24 08:33:03 +0530

    Add files via upload

--- python/dict.py
+++ python/dict.py
@@ -1,4 +1,8 @@
-keys=input("Enter the keys of dictionaty").split()
-values=input("Enter the values of the dictionary:").split()
-print(tuple(zip(keys,values)))
-
+dict1={}
+n=int(input("Enter the elemnts in the dictionary:"))
+for i in range(n):
+    key=input("Enter the key :")
+    value=input("Enter the value:")
+    dict1[key]=value
+sorted_dict=sorted(dict1.value(),reverse =True)
+print("\nThe sorted dictionary is:\n",sorted_dict)
