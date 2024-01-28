5d554e67ff6aa2fb611306f0308dcb067229c9e1
Arungeorge103 <153169302+Arungeorge103@users.noreply.github.com>
2024-01-29 04:12:49 +0530

    Add files via upload

---
+++ sample3/homeframes.php
@@ -0,0 +1,10 @@
+<html>
+    <head>
+        <title> Home </title>
+    </head>
+<frameset cols= '20%,80%'>
+        <frame src="menu.php" name="frame1">
+        <frame src="home1.php" name="frame2">
+</frameset>
+</html>
+
---
+++ sample3/menu.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>Menu</title>
+    </head>
+    <body>
+        <ul>
+            <li><a href="home1.php" target="frame2">Home</a></li>
+            <li><a href="log.php" target="frame2">Login</a></li>
+            <li><a href="ktu_reg.php" target="frame2">KTU Register</a></li>
+            <li><a href="marken.php" target="frame2">Mark Entry</a></li>
+        </ul>
+    </body>
+</html>
